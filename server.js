bc246d24e07d555887897cc1260e9bddf19a7476
CarlosPerzS <carlosps2809@gmail.com>
2025-05-27 18:19:35 -0600

    registros en la bd

--- server.js
+++ server.js
@@ -12,13 +12,30 @@ app.post('/login', (req,res)=>{
     db.query(sql,[usuario, contrasena], (err, resultados)=>{
         if(err)throw err;
         if(resultados.length>0){
-            res.send('Inicio de sesion exitoso');
+            res.redirect('/registro.html');
         }
         else{
             res.send("Credenciales incorrectas");
         }
     });
 });
+app.post('/insert', (req, res)=>{
+    const {usuario,contrasena}=req.body;
+    const verificar = 'SELECT * FROM Usuarios WHERE usuario= ?;';
+    db.query(verificar, [usuario], (err, resultados)=>{
+        if(err)throw err;
+        if(resultados.length>0){
+            res.send('Usuario existente');
+        }
+        else{
+            const sql = 'INSERT INTO Usuarios (usuario, contrasena) VALUES (?, ?);';
+            db.query(sql,[usuario,contrasena], (err)=>{
+            if(err)throw err;
+            res.send('Registro exitoso');
+            })
+        }
+    })
+})
 app.listen(3000,()=>{
     console.log('Servidor corriendo en http://localhost:3000');
 });
